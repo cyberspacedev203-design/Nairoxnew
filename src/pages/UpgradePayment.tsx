05cf421b11fd9d4c6b09157d34ab230d4f89a763
cyberspacedev203-design <cyberspace.dev203@gmail.com>
2026-05-28 11:35:02 +0100

    Update UpgradePayment.tsx

--- src/pages/UpgradePayment.tsx
+++ src/pages/UpgradePayment.tsx
@@ -120,11 +120,11 @@ const UpgradePayment = () => {
               <p className="text-sm font-semibold">Bank Details</p>
               <div className="space-y-1 text-sm">
                 <div className="flex items-center justify-between">
-                  <p className="font-mono">Account: 1097436921</p>
-                  <CopyButton text="1097436921" />
+                  <p className="font-mono">Account: 2065669422</p>
+                  <CopyButton text="2065669422" />
                 </div>
                 <p>Name: David Odum</p>
-                <p>Bank: paga bank</p>
+                <p>Bank: kuda bank</p>
               </div>
             </div>
             <NarrationSection text="707404" />
